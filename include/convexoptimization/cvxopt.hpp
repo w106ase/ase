d4eb2853e2386e8fe1afcf2d6fd970174657d4e7
Aaron Evers <aaron.evers@mr-c02tq1jthtd9.attlocal.net>
2018-05-03 08:43:14 -0400

    Added documentation for the backtracking line searches, updated the todo list, and added a bib file for citations.

--- include/convexoptimization/cvxopt.hpp
+++ include/convexoptimization/cvxopt.hpp
@@ -1,21 +1,53 @@
+/*! \file cvxopt.hpp
+    \brief Convex optimization functionality using Intel MKL functionality.
+
+    Convex optimization functionality using Intel MKL functionality. This
+    functionality includes line search routines, gradient descent routines,
+    steepest descent routines, Newton's method routines, and various generic and
+    specialized solvers for convex optimization problems. Much of the provided
+    functionality is project driven with the application being radar signal
+    processing. Thus, where appropriate routines exist for handling both real-
+    and complex-valued inputs.
+*/
+
 #ifndef CVXOPT_H
 #define CVXOPT_H
 #include <complex>
 #include <functional>
 #include <vector>
 
-/*! \file cvxopt.hpp
-    \brief Convex optimization functionality using Intel MKL functionality.
-
-    Details.
-*/
-
 namespace ase
 {
+  /*! \fn double backtracking_line_search( const std::function< double ( std::vector< double > dx ) >& f_obj,
+                                           const std::vector< double > grad_f_obj_at_x, std::vector< double > dx,
+                                           const double f_obj0, const double alpha = 0.45, const double beta = 0.8, const int max_iter = 100 )
+      \brief Backtracking line search (see \cite Boyd2004_ase Pg. 464) for determining a step-size, which minimizes a provided objective function that takes real-valued arguments.
+
+      \param f_obj objective function which takes a single, real-valued argument <tt>dx</tt>, and evaluates <tt>f_obj(x+dx)</tt>.
+      \param grad_f_obj_at_x gradient of the objective function at the current location <tt>x</tt>
+      \param dx descent direction to determine a step-size for (overwritten with <tt>step_size*dx</tt>).
+      \param f_obj0 initial objective function value at <tt>x</tt> (i.e., <tt>f_obj(x)</tt>).
+      \param alpha backtracking line search parameter (see \cite Boyd2004_ase Pg. 464).
+      \param beta backtracking line search parameter (see \cite Boyd2004_ase Pg. 464).
+      \param max_iter maximum number of iterations to apply the backtracking line search.
+  */
   double backtracking_line_search( const std::function< double ( std::vector< double > dx ) >& f_obj,
                                    const std::vector< double > grad_f_obj_at_x, std::vector< double > dx,
                                    const double f_obj0, const double alpha = 0.45, const double beta = 0.8, const int max_iter = 100 );
 
+  /*! \fn double backtracking_line_search( const std::function< double ( std::vector< std::complex< double > > dx ) >& f_obj,
+                                           const std::vector< std::complex< double > > grad_f_obj_at_x, std::vector< std::complex< double > > dx,
+                                           const double f_obj0, const double alpha = 0.45, const double beta = 0.8, const int max_iter = 100 )
+      \brief Backtracking line search (see \cite Boyd2004_ase Pg. 464) for determining a step-size, which minimizes a provided objective function that takes complex-valued arguments.
+
+      \param f_obj objective function which takes a single, complex-valued argument <tt>dx</tt>, and evaluates <tt>f_obj(x+dx)</tt>.
+      \param grad_f_obj_at_x gradient of the objective function at the current location <tt>x</tt>
+      \param dx descent direction to determine a step-size for (overwritten with <tt>step_size*dx</tt>).
+      \param f_obj0 initial objective function value at <tt>x</tt> (i.e., <tt>f_obj(x)</tt>).
+      \param alpha backtracking line search parameter (see \cite Boyd2004_ase Pg. 464).
+      \param beta backtracking line search parameter (see \cite Boyd2004_ase Pg. 464).
+      \param max_iter maximum number of iterations to apply the backtracking line search.
+  */
   double backtracking_line_search( const std::function< double ( std::vector< std::complex< double > > dx ) >& f_obj,
                                    const std::vector< std::complex< double > > grad_f_obj_at_x, std::vector< std::complex< double > > dx,
                                    const double f_obj0, const double alpha = 0.45, const double beta = 0.8, const int max_iter = 100 );
--- src/convexoptimization/cvxopt.cpp
+++ src/convexoptimization/cvxopt.cpp
@@ -1,5 +1,4 @@
 #include <complex>
-#include <iostream>
 #include <functional>
 #include <vector>
 #include "convexoptimization/cvxopt.hpp"
@@ -12,13 +11,12 @@ namespace ase
                                    const double f_obj0, const double alpha, const double beta, const int max_iter )
   {
     // Pre-allocations/-calculations.
-    double step_size = 1.0, dx0 = dx[ 0 ], f_obj_new = f_obj( dx );
+    double step_size = 1.0, f_obj_new = f_obj( dx );
     int iter = 0, n = dx.size( );
     double grad_f_obj_dx = cblas_ddot( n, grad_f_obj_at_x.data( ), 1, dx.data( ), 1 );
 
     while( f_obj_new > ( f_obj0+step_size*alpha*grad_f_obj_dx ) && iter < max_iter )
     {
-      std::cout << "In loop" << std::endl;
       // Evaluate the objective function using the current step-size.
       cblas_dscal( n, beta, dx.data( ), 1 );
       f_obj_new = f_obj( dx );
@@ -26,13 +24,32 @@ namespace ase
       // Increment the counter.
       iter += 1;
     }
-    return dx[ 0 ]/dx0;
+    return pow( beta, iter );
   }
 
   double backtracking_line_search( const std::function< double ( std::vector< std::complex< double > > dx ) >& f_obj,
                                    const std::vector< std::complex< double > > grad_f_obj_at_x, std::vector< std::complex< double > > dx,
                                    const double f_obj0, const double alpha, const double beta, const int max_iter )
   {
-    return 0.0;
+    // Pre-allocations/-calculations.
+    double step_size = 1.0, f_obj_new = f_obj( dx );
+    int iter = 0, n = dx.size( );
+
+    /* NOTE: this quantity should always be real-valued. To enforce this only
+    the real portion is retained. */
+    std::complex< double > grad_f_obj_dx_cplx;
+    cblas_zdotc_sub( n, grad_f_obj_at_x.data( ), 1, dx.data( ), 1, &grad_f_obj_dx_cplx );
+    double grad_f_obj_dx = real( grad_f_obj_dx_cplx );
+
+    while( f_obj_new > ( f_obj0+step_size*alpha*grad_f_obj_dx ) && iter < max_iter )
+    {
+      // Evaluate the objective function using the current step-size.
+      cblas_zdscal( n, beta, dx.data( ), 1 );
+      f_obj_new = f_obj( dx );
+
+      // Increment the counter.
+      iter += 1;
+    }
+    return pow( beta, iter );
   }
 }
